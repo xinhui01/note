8d0392875c95e238310c2d13deac72b72350117b
xinhui <xinhui01@foxamil>
2021-06-30 20:52:19 +0800

    add

---
+++ mysql/sql.sql
@@ -0,0 +1,2 @@
+-- 按时间分组统计
+select from_unixtime(createtime , '%Y-%m-%d %h') date, sum(energy)  from tl_chat_log where createtime >= UNIX_TIMESTAMP('2021-06-29 00:00:00') and createtime <= UNIX_TIMESTAMP('2021-06-30 00:00:00') and to_user_id = 69272 group by date
