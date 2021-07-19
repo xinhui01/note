6dc938bd07a31001e66945ade3d12978ac168290
xinhui <xinhui01@foxamil>
2021-07-19 16:59:54 +0800

    add

--- mysql/sql.sql
+++ mysql/sql.sql
@@ -1,2 +1,4 @@
 -- 按时间分组统计
 select from_unixtime(createtime , '%Y-%m-%d %h') date, sum(energy)  from tl_chat_log where createtime >= UNIX_TIMESTAMP('2021-06-29 00:00:00') and createtime <= UNIX_TIMESTAMP('2021-06-30 00:00:00') and to_user_id = 69272 group by date
+-- 修改自增的值
+alter table tl_roomid_list AUTO_INCREMENT=100000000;
